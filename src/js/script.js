b77e1ff527498f30e7088217ad2be5544c3f23dc
chiehaya <jakkiriko@yahoo.co.jp>
2023-10-22 16:12:28 +0900

    2回目

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,32 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    // ドロワーメニュー
+    const hamburger = $(".js-hamburger");
+    hamburger.on("click", function () {
+        if ($('.js-hamburger').hasClass('is-open')){
+            $('.js-sp-nav').fadeOut();
+            $(this).removeClass('is-open');
+        }else {
+            $('.js-sp-nav').fadeIn();
+            $(this).addClass('is-open')
+        }
+    });
+    // swiper
+    const swiper = new Swiper(".js-swiper",{
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay:  3000
+        },
+        effect: 'fade',
+    
+    });
 
 });
